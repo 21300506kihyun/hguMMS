c6a94227498c39fd47cf78515b3af0d8ca396183
JangJuMan <JangJuMan@github.io>
2020-02-12 23:09:16 +0900

    개인정보 수정화면 DB 연동하기 완료

--- html/personalInfo.php
+++ html/personalInfo.php
@@ -18,18 +18,36 @@
         obj.innerHTML = "ON";
       }
     }
+    function check(){
+      var frm = document.form;
+      var phone_number = frm.phone.value;
+      var office_place = frm.office.value;
+      var val_sheet = false;
+      var val_prof = false;
+      var sheet = document.getElementById('is_sheet');
+      var prof = document.getElementById('is_prof');
+      if(sheet.checked){
+        val_sheet = true;
+      }
+      else{
+        val_sheet = false;
+      }
+
+      if(prof.checked){
+        val_prof = true;
+      }
+      else{
+        val_prof = false;
+      }
+
+
+      alert(val_sheet);
+      alert(val_prof);
+
+
+
+    }
   </script>
-  <?php
-  session_start();
-  if($_POST['name1'] !== NULL){
-    $name = $_POST['name1'];
-    $_SESSION["name"] = $name;
-    $email = $_POST['email1'];
-    $_SESSION["email"] = $email;
-    $img = $_POST['img1'];
-    $_SESSION["img"] = $img;
-  }
-  ?>
   <title>handongMMS</title>
 </head>
 <body>
@@ -38,6 +56,26 @@
     <br>
     <p>한동대학교 면담 예약 및 관리 시스템입니다.</p>
   </div>
+
+  <?php
+    session_start();
+    $name = $_SESSION["name"];
+    $email = $_SESSION['email'];
+    $img = $_SESSION['img'];
+
+    $conn = new mysqli("localhost", "hgumms", "handong11*");
+    mysqli_select_db($conn, 'hgumms');
+    $result = mysqli_query($conn, "select * from user_info where email='$email'");
+
+    $data = mysqli_fetch_assoc($result);
+      echo $data['sid'];
+      echo $data['name'];
+      echo $data['prof'];
+      echo $data['email'];
+      echo $data['phone'];
+      echo $data['office'];
+      echo "<br/>";
+  ?>
 
   <div class="row">
     <div class="column left">
@@ -58,33 +96,41 @@
       <input class="btn_left" type="button" value="로그아웃"/>
     </div>
 
+<form action="./personalInfoController.php" method="post" name="form" >
     <div class="column middle">
       <h2 class="subtitle">개인정보 수정</h2>
       <div class="content">
         <h3 class="category">이름</h3>
         <h3 class="deli">:</h3>
-        <input class="value" type="text" value="<?php echo $_SESSION['name']?>">
+        <input name="name" id="name" class="value" type="text" value="<?php echo $name ?>" readonly>
       </div>
       <div class="content">
         <h3 class="category">전화번호</h3>
         <h3 class="deli">:</h3>
-        <input class="value" type="text" value="010-1234-1234">
+        <input name="phone" id="phone" class="value" type="text" placeholder="ex) 010-0000-0000" value="<?php echo $data['phone'] ?>">
       </div>
       <div class="content">
         <h3 class="category">오피스 위치</h3>
         <h3 class="deli">:</h3>
-        <input class="value" type="text" value="DB.office.where">
+        <input name="office" id="office" class="value" type="text" placeholder="ex) NTH 415 or None" value="<?php echo $data['office']?>">
       </div>
       <div class="content">
         <h3 class="category" style="width:85%">개인 면담시트 개설 여부</h3>
         <h3 class="deli">:</h3>
-        <input class="checkbox" type="checkbox" value="is_sheet" checked>
+        <input name="is_sheet" class="checkbox" type="checkbox" id="is_sheet" checked>
+      </div>
+      <div class="content">
+        <h3 class="category" style="width:85%">면담 제공 여부</h3>
+        <h3 class="deli">:</h3>
+        <input name="is_prof" class="checkbox" type="checkbox" id="is_prof" checked>
       </div>
 
       <div class="btns">
-          <input class="btn_back" type="button" value="돌아가기"/>
-          <input class="btn_save" type="button" value="변경 사항 저장하기"/>
+          <input class="btn_back" type="button" value="돌아가기" onclick="location.href='main.php'"/>
+          <input class="btn_save" type="submit" value="변경 사항 저장하기"/>
       </div>
+</form>
+
 
     </div>
   </div>
